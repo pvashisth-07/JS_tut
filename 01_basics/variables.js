f6f40ea5f09b541962a11e225d48b9aded1c65e3
pvashisth-07 <pvashisth0711@gmail.com>
2026-06-11 00:57:15 +0530

    varaibles

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=12345 //constant
+
+let email="pranav@pranav.com"
+var pass="12345"
+city="Jaipur" //variable can be declared without specifying
+
+//var and let both are used to declare variable but var doesn't know about the scope of variable but let does, so we use let
+let vari
+
+console.log(accountId);
+console.table([accountId, email, pass, city, vari]);
