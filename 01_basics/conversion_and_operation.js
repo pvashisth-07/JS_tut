534813c72e7a4dd075eef451f67aa8a4afb6beab
pvashisth-07 <pvashisth0711@gmail.com>
2026-06-14 00:20:45 +0530

    Conversions and Datatype

---
+++ 01_basics/conversion_and_operation.js
@@ -0,0 +1,15 @@
+let score=33
+//console.log(typeof score)
+
+// let val="11a"
+// let valInNum=Number(val);
+// console.log(typeof valInNum);
+// console.log(valInNum);
+//null=>0, String purely no. =>number, true/false=>0/1, rest=>NaN
+
+
+// let val2=-1;
+// let boolval=Boolean(val2)
+// console.log(boolval);
+//only 0,null,empty is bool false rest all true
+
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat the entire code as newer JS verison
+
+//alert(msg) on browser this give a pop showing the message
+
+//tc39.es JS documentation
+//easy version refer to mdn
+
+let name="Pranav"
+let age=18;
+
+//number 2^53
+//bigint
+//string -> ""
+//boolen-> t/f
+// null => standalone value of empty
+// undefined => value not yet assigned
+//symbol =>for uniqueness
+
+//Non  primitive data type:
+//object
+
+//to check typeof (typeof var)
